6056a9141e462e7b93afa5cff7393bb892558fcf
sofia-zamiatina <son22zam@gmail.com>
2024-07-25 18:25:48 +0500

    projects section started

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function openProject(projectId) {
+    document.getElementById(projectId).style.display = "block";
+}
+
+function closeProject(projectId) {
+    document.getElementById(projectId).style.display = "none";
+}
